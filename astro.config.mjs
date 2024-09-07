106b67e5f4bbfa2e57541cc357e8dfff51d610b6
installer <karthikssalian5@gmail.com>
2024-09-07 18:35:48 +0530

    resolved confilict

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,7 @@ export default defineConfig({
   integrations: [tailwind(), react(), icon({}), auth()],
   output: 'server',
   adapter: vercel(),
+  experimental:{
+    actions:true
+  }
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
-
 declare namespace App {
   interface Locals {
     email: string | null | undefined;
     name: string | null | undefined;
     role: string | null | undefined;
   }
+}
+interface Window {
+  gsap: typeof import('gsap').gsap;
+  ScrollTrigger: typeof import('gsap/ScrollTrigger').ScrollTrigger;
+  gsapContext: ReturnType<typeof import('gsap').gsap.context>;
 }
