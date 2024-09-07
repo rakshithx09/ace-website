a86aaea36c3cb747bf793e65c10195faf5610c0c
Ananth Shetty <ananthshetty203@gmail.com>
2024-09-07 07:29:34 +0530

    changes

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
+console.log( import.meta.env.TURSO_CONNECTION_URL, import.meta.env.TURSO_AUTH_TOKEN)
+
 const client = createClient({ url: import.meta.env.TURSO_CONNECTION_URL, authToken: import.meta.env.TURSO_AUTH_TOKEN });
 export const db = drizzle(client);
